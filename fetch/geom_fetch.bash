f029eb87d534fc2bba9282eb904d2032c562e8fb
Michael Christoffersen <mchristo28@gmail.com>
2020-04-15 23:27:00 -0700

    Fixed fetch scripts

--- fetch/geom_fetch.bash
+++ fetch/geom_fetch.bash
@@ -2,6 +2,9 @@
 
 # First arg is a sharad number in the format s_00168901
 
+pfix=/home/mchristo/proj/simc/test/nav/geom/
 baseurl="http://pds-geosciences.wustl.edu/mro/mro-m-sharad-5-radargram-v1/mrosh_2001/data/geom/"
+
 s=`echo $1 | cut -c 1-6`
-wget -P /zippy/MARS/code/modl/simc_sharad/test/temp/nav/geom/ -nc "${baseurl}${s}xx/${1}_geom.tab"
+
+wget -P $pfix -nc "${baseurl}/${s}xx/${1}_geom.tab"
--- fetch/hrsc_fetch.bash
+++ fetch/hrsc_fetch.bash
@@ -2,6 +2,9 @@
 
 # First arg is a HRSC label in the format h2179_0000
 
+pfix=/home/mchristo/proj/simc/test/dem/hrsc/
 baseurl="http://pds-geosciences.wustl.edu/mex/mex-m-hrsc-5-refdr-dtm-v1/mexhrs_2001/data/"
+
 s=`echo $1 | cut -c 2-5`
-wget -P /zippy/MARS/code/modl/MRO/simc/test/temp/dem/hrsc/ -nc "${baseurl}${s}/${1}_da4.img"
+
+wget -P $pfix -nc "${baseurl}/${s}/${1}_da4.img"
