9899dbf9680774684c0855714dfbc171e0a26378
Michael <mchristo28@gmail.com>
2021-02-02 20:07:53 -0700

    fetch label fixes

--- fetch/label_fetch.bash
+++ fetch/label_fetch.bash
@@ -2,7 +2,7 @@
 
 # First arg is a sharad number in the format s_00168901
 
-savedir=/home/mchristo/proj/simc/label
+savedir=/zippy/MARS/code/modl/simc/label/
 
 baseurl="http://pds-geosciences.wustl.edu/mro/mro-m-sharad-5-radargram-v1/mrosh_2001/data/rgram/"
 s=`echo $1 | cut -c 1-6`
